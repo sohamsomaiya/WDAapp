3bd9f9eabda0624f06fdbc8d9c702d4e69418eb1
Darshi2412 <126317340+Darshi2412@users.noreply.github.com>
2023-10-05 00:37:50 -0700

    Add files via upload

---
+++ app/src/main/java/com/example/wda/model/Profile.kt
@@ -0,0 +1,14 @@
+package com.example.myapplication.model
+
+class Profile (
+        var Name : String,
+        var Type : String,
+        var ContactNo : String,
+        var Gender : String?,
+        var Dob : String?,
+        var Address : String?,
+        var City : String?,
+        var State : String?,
+        var Pincode : String?,
+        var imageName:String?
+        )
---
+++ app/src/main/java/com/example/wda/model/Website.kt
@@ -0,0 +1,11 @@
+package com.example.wda.model
+
+class Website (
+        var Id : String,
+        var WebsiteName : String,
+        var WebsiteType : String,
+        var DateOfIntegration : String,
+        var CorporateIdentificationNo :String,
+        var TaxDeductionAccNo : String,
+        var DomainName : String
+        )
