11ec74fb389d56b6ea31a94464b89052bf5f6516
sohamsomaiya <somaiyasoham@gmail.com>
2023-10-15 22:31:14 +0530

    view query grid

--- app/src/main/java/com/example/wda/adapter/ViewQueryAdapter.kt
+++ app/src/main/java/com/example/wda/adapter/ViewQueryAdapter.kt
@@ -7,11 +7,10 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.TextView
 import com.example.wda.R
-import com.example.wda.model.UserWebsite
-import com.example.wda.model.Website
+import com.example.wda.model.Queries
 
-class DropdownWebNameAdapter (private val activity: Activity, private val objects: Array<UserWebsite>) :
-    ArrayAdapter<UserWebsite>(activity, R.layout.dropdowngrid
+class ViewQueryAdapter(private val activity: Activity, private val objects: Array<Queries>) :
+    ArrayAdapter<Queries>(activity, R.layout.dropdowngrid
         , objects) {
     @SuppressLint("SetTextI18n")
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
@@ -21,27 +20,33 @@ class DropdownWebNameAdapter (private val activity: Activity, private val object
         if (view == null) {
             view = activity.layoutInflater.inflate(R.layout.dropdowngrid, parent, false)
             viewHolder = ViewHolder()
-            viewHolder.hiddenText = view.findViewById(R.id.HiddenTextSubjectId)
             viewHolder.WebsiteName = view.findViewById(R.id.dropDownItemText)
+            viewHolder.WebsiteId = view.findViewById(R.id.WebsiteIdTxt)
+            viewHolder.QueryDate = view.findViewById(R.id.QueryDate)
+            viewHolder.QueryDescription = view.findViewById(R.id.QueryDescription)
 
             view.tag = viewHolder
         } else
             viewHolder = view.tag as ViewHolder
 
-        viewHolder.hiddenText.text = objects[position].WebsiteId
+        viewHolder.WebsiteId.text = objects[position].WebsiteId
+        viewHolder.QueryDate.text = objects[position].Date
         viewHolder.WebsiteName.text = objects[position].WebsiteName
+        viewHolder.QueryDescription.text = objects[position].Description
 
-        fun getWebsiteId(position: Int): String {
-            return objects[position].WebsiteId!!
-        }
+//        fun getWebsiteId(position: Int): String {
+//            return objects[position].WebsiteId!!
+//        }
 
         return view!!
     }
 
     companion object {
         class ViewHolder {
-            lateinit var hiddenText: TextView
             lateinit var WebsiteName: TextView
+            lateinit var WebsiteId: TextView
+            lateinit var QueryDate: TextView
+            lateinit var QueryDescription: TextView
         }
     }
 }
