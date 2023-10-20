02967ececaae2ce76124e332b84eafb782e035d8
sohamsomaiya <somaiyasoham@gmail.com>
2023-10-20 23:08:23 +0530

    Create site color and font S2

--- app/src/main/java/com/example/wda/CreateWebActivity.kt
+++ app/src/main/java/com/example/wda/CreateWebActivity.kt
@@ -112,8 +112,6 @@ class CreateWebActivity : AppCompatActivity() {
                     }
                 }
             }
-            val intent=Intent(this@CreateWebActivity,TemplateSelectionActivity::class.java)
-            startActivity(intent)
 
         }
     }
